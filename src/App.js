e9139d76c77f03920abb1ca64732504b4a228165
Boris Hofer <boris.hofer@liip.ch>
2022-03-17 14:01:19 +0100

    Os Detection provider

--- src/App.js
+++ src/App.js
@@ -6,44 +6,47 @@ import ThirdPage from "./pages/ThirdPage";
 import FourthPage from "./pages/FourthPage";
 import Dashboard from "./pages/Dashboard";
 import Login from "./components/Login";
-import useToken from "./hooks/useToken";
+import useToken from "./hooks/useToken.js";
 import Logged from "./pages/Logged";
 import IconProvider from "./providers/IconProvider";
+import OsDetectionProvider from "./providers/OsDetectionProvider";
 
 function App() {
 
   const { setToken } = useToken()
 
   return (
-    <IconProvider>
-      <div className="app">
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/">
-              <Homepage />
-            </Route>
-            <Route exact path="/newsfeed">
-              <SecondPage />
-            </Route>
-            <Route exact path="/profile">
-              <Logged />
-            </Route>
-            <Route exact path="/currencyconverter">
-              <ThirdPage />
-            </Route>
-            <Route exact path="/both">
-              <FourthPage />
-            </Route>
-            <Route exact path="/dashboard">
-              <Dashboard />
-            </Route>
-            <Route exact path="/login">
-              <Login setToken={setToken} />
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </div>
-    </IconProvider>
+    <OsDetectionProvider>
+      <IconProvider>
+        <div className="app">
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/newsfeed">
+                <SecondPage />
+              </Route>
+              <Route exact path="/profile">
+                <Logged />
+              </Route>
+              <Route exact path="/currencyconverter">
+                <ThirdPage />
+              </Route>
+              <Route exact path="/both">
+                <FourthPage />
+              </Route>
+              <Route exact path="/dashboard">
+                <Dashboard />
+              </Route>
+              <Route exact path="/login">
+                <Login setToken={setToken} />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </IconProvider>
+    </OsDetectionProvider>
   );
 }
 
--- src/pages/Logged.js
+++ src/pages/Logged.js
@@ -4,6 +4,37 @@ import Navbar from '../components/Navbar'
 import SideNav from '../ui/SideNav'
 
 function Logged() {
+  const os = localStorage.getItem('os')
+
+  console.log('OS: ', os)
+
+  /*
+  if (os === 'iOs') {
+    // create ios share buttons
+  } else if (os === 'Android') {
+    // create android share buttons
+  } else if (os === 'Windows Phone') {
+    // create windows phone share buttons
+  } else {
+    // create desktop share buttons
+  }
+  */
+
+  switch (os) {
+    case 'iOS':
+      // create ios share buttons
+      break;
+    case 'Android':
+      // create android share buttons
+      break;
+    case 'Windows Phone':
+      // create windows phone share buttons
+      break;
+    default:
+      // create desktop share buttons
+  }
+
+
   return (
  <>
  <Navbar />
